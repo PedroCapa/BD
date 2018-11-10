384f2afc0606e39a47c0ce69209ea7b1c2c605c6
Moisés Antunes <36673656+MoisesA14@users.noreply.github.com>
2018-11-10 22:38:33 +0000

    PovoamentoContactos

---
+++ PovoamentoContactos.sql
@@ -0,0 +1,20 @@
+Use mercado;
+
+SELECT * FROM contactos;
+
+INSERT INTO contactos
+	VALUES('912345678', 0, 1),
+		  ('lfcc@gmail.com', 1, 2),
+          ('ma14@hotmail.com', 1, 3),
+          ('pmcc@gmail.com', 1, 4),
+          ('922992299', 0, 5),
+          ('maregolo@hotmail.com', 1, 6),
+          ('999999999', 0, 7),
+          ('var@roubo.com', 1, 8),
+          ('987654321', 0, 9),
+          ('951668866', 0, 10),
+          ('galaxys7@granada.com', 1, 11),
+          ('926222652', 0, 12),
+          ('carro@luxo.com', 1, 13),
+          ('viatura@dinheiro.com', 1, 14),
+          ('901000000', 0, 15);
