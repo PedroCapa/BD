a99baeacd891a35bced9404b9c4ccd947fcf3293
LFCC98 <a81960@alunos.uminho.pt>
2018-11-14 16:43:43 +0000

    PovoamentoProduto

--- PovoamentoProduto.sql
+++ PovoamentoProduto.sql
@@ -1,25 +1,25 @@
 USE mercado;
 
 -- Inserir alguns produtos na base de dados
-INSERT INTO produto (idProduto, Designacao, Descricao, Preco, Categoria)
-	VALUES (1, 'Mochila', 'Mochila cinzenta e nova', '29.99', 'Material escolar'),
-		   (2, 'Relógio', 'Relógio Rolex em ouro', '7000', 'Acessório'),
-		   (3, 'Mesa', 'Mesa Branca', '25', 'Móvel'),
-           (4, 'Caderno', 'Caderno de linhas', '0.99', 'Material escolar'),
-           (5, 'Quadro Mona Lisa', 'Mona Lisa, de Leonardo da Vinci', '1000000', 'Arte'),
-           (6, 'Conjunto de canetas', 'Canetas de cor azul', '10.25', 'Material escolar'),
-           (7, 'Casaco', 'Casaco Branco', '99.99', 'Vestuário'),
-           (8, 'Camisola', 'Camisola do Futebol Clube do Porto', '79.99', 'Equipamento desportivo'),
-           (9, 'Cachecol', 'Cachecol do Futebol Clube do Porto com assinatura de Marega', '19.89', 'Vestuário'),
-           (10, 'Borracha', 'Borracha branca da marca STAEDTLER', '0.45', 'Material escolar'),
-           (11, 'Carteira', 'Carteira Tommy Hilfiger', '150', 'Acessório'),
-           (12, 'Busto Cristiano Ronaldo', 'Busto do melhor jogador do mundo de futebol', '1.99', 'Arte'),
-           (13, 'Camisola', 'Camisola azul e branca', '79.99', 'Vestuário'),
-           (14, 'Caneta de minas', 'Lapiseira Rotring', '0.7', 'Material escolar'),
-           (15, 'Relógio Swatch', 'Relógio de luxo Swatch', '2500', 'Acessório'),
-           (16, 'T-Shirt TH', 'T-Shirt Tommy Hilfiger branca', '60.99', 'Vestuário'),
-           (17, 'Samsung Galaxy s8', 'Smartphone em 2ª mão', '150.99', 'Smartphone'),
-           (18, 'Fones Samsung', 'Fones de ouvido Samsung', '12.29', 'Técnologia'),
-           (19, 'Pintura A Última Ceia', 'Pintura Leonardo da Vinci', 'Arte'),
-           (20, 'Samsung Galaxy Note9', 'Smartphone preto e novo', '999.99', 'Smartphone');
+INSERT INTO produto
+	VALUES (1, 'Mochila', 'Mochila cinzenta e nova', '29.99', 'Material escolar', '2', '10'),
+		   (2, 'Relógio', 'Relógio Rolex em ouro', '7000', 'Acessório', '5', '1'),
+		   (3, 'Mesa', 'Mesa Branca', '25', 'Móvel', '2','1'),
+           (4, 'Caderno', 'Caderno de linhas', '0.99', 'Material escolar', '3','50'),
+           (5, 'Quadro Mona Lisa', 'Mona Lisa, de Leonardo da Vinci', '100000', 'Arte', '6','1'),
+           (6, 'Conjunto de canetas', 'Canetas de cor azul', '10.25', 'Material escolar', '8', '50'),
+           (7, 'Casaco', 'Casaco Branco', '99.99', 'Vestuário', '6', '5'),
+           (8, 'Camisola', 'Camisola do Futebol Clube do Porto', '79.99', 'Equipamento desportivo', '8','1'),
+           (9, 'Cachecol', 'Cachecol do Futebol Clube do Porto com assinatura de Marega', '19.89', 'Vestuário',  '6','5'),
+           (10, 'Borracha', 'Borracha branca da marca STAEDTLER', '0.45', 'Material escolar', '2','20'),
+           (11, 'Carteira', 'Carteira Tommy Hilfiger', '150', 'Acessório', '5', '3'),
+           (12, 'Busto Cristiano Ronaldo', 'Busto do melhor jogador do mundo de futebol', '1.99', 'Arte', '5', '1'),
+           (13, 'Camisola', 'Camisola azul e branca', '79.99', 'Vestuário', '5', '3'),
+           (14, 'Caneta de minas', 'Lapiseira Rotring', '0.7', 'Material escolar', '6', '5'),
+           (15, 'Relógio Swatch', 'Relógio de luxo Swatch', '2500', 'Acessório', '5', '1'),
+           (16, 'T-Shirt TH', 'T-Shirt Tommy Hilfiger branca', '60.99', 'Vestuário', '2','3'),
+           (17, 'Samsung Galaxy s8', 'Smartphone em 2ª mão', '150.99', 'Smartphone', '11', '1500'),
+           (18, 'Fones Samsung', 'Fones de ouvido Samsung', '12.29', 'Técnologia', '11', '150'),
+           (19, 'Pintura A Última Ceia', 'Pintura Leonardo da Vinci', '500000', 'Arte', '1', '1'),
+           (20, 'Samsung Galaxy Note9', 'Smartphone preto e novo', '999.99', 'Smartphone', '11', '100');
            
