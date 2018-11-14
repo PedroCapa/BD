84a11263666cb320c7f6b56f0fbafc76e65db50b
Moisés Antunes <36673656+MoisesA14@users.noreply.github.com>
2018-11-14 17:08:08 +0000

    Contactos

--- PovoamentoContactos.sql
+++ PovoamentoContactos.sql
@@ -1,20 +1,24 @@
 Use mercado;
 
-SELECT * FROM contactos;
+SELECT * FROM telemovel;
 
-INSERT INTO contactos
-	VALUES('912345678', 0, 1),
-		  ('lfcc@gmail.com', 1, 2),
-          ('ma14@hotmail.com', 1, 3),
-          ('pmcc@gmail.com', 1, 4),
-          ('922992299', 0, 5),
-          ('maregolo@hotmail.com', 1, 6),
-          ('999999999', 0, 7),
-          ('var@roubo.com', 1, 8),
-          ('987654321', 0, 9),
-          ('951668866', 0, 10),
-          ('galaxys7@granada.com', 1, 11),
-          ('926222652', 0, 12),
-          ('carro@luxo.com', 1, 13),
-          ('viatura@dinheiro.com', 1, 14),
-          ('901000000', 0, 15);
+INSERT INTO telemovel
+	VALUES('912345678', 1),
+          ('922992299', 5),
+          ('999999999', 7),
+          ('987654321', 9),
+          ('951668866', 10),
+          ('926222652', 12),
+          ('901000000', 15);
+          
+SELECT * FROM email;
+
+INSERT INTO email
+	VALUES('lfcc@gmail.com', 2),
+          ('ma14@hotmail.com', 3),
+          ('pmcc@gmail.com', 4),
+          ('maregolo@hotmail.com', 6),
+          ('var@roubo.com', 8),
+          ('galaxys7@granada.com', 11),
+          ('carro@luxo.com', 13),
+          ('viatura@dinheiro.com', 14);
