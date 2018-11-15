97ba89853989eaa3ea313f523ccf37cab7fc18f5
PedroCapa <36596171+PedroCapa@users.noreply.github.com>
2018-11-15 11:57:03 +0000

    Alterações no esquema Logico

--- Povoamento.sql
+++ Povoamento.sql
@@ -2,17 +2,17 @@ Use mercado;
 
 INSERT INTO utilizador 
 	VALUES(1, 'Lucas Peixoto Silva', 'Rua dos Gritos nº1', '2012-01-17'),
-		  (2, 'Luís Filipe da Costa Capa', 'Estádio do Dragão', '1998-12-01'),
-		  (3, ' Moisés Araújo Antunes', 'Estádio do Dragão', '1999-04-14'),
-		  (4, 'Pedro Miguel da Costa Capa', 'Estádio do Dragão', '1998-12-01'),
-          (5, 'Tiago Martins Pinheiro', 'Estádio do Dragão', '1998-08-06'),
-          (6, 'Moussa Marega', 'Estádio do Dragão', '1991-04-14'),
+		  (2, 'Luís Filipe da Costa Capa', 'Vila Verde', '1998-12-01'),
+		  (3, ' Moisés Araújo Antunes', 'Maria da Fonte', '1999-04-14'),
+		  (4, 'Pedro Miguel da Costa Capa', 'Vila Verde', '1998-12-01'),
+          (5, 'Tiago Martins Pinheiro', 'Vila Verde', '1998-08-06'),
+          (6, 'Moussa Marega', 'Vila Verde', '1991-04-14'),
           (7, 'Rodrigo Rodrigues', 'Braga', '1900-01-01'),
           (8, 'Pedro Proença', 'Lisboa', '1970-04-06'),
           (9, 'Gabriel Toledo', 'New York', '1991-05-30'),
           (10, 'Epitacio de Melo', 'New York', '1995-07-04'),
           (11, 'SAMSUNG', ' Seul Coreia do Sul', '1938-01-01'),
-          (12, 'Coca-Cola', 'Atlanta Estados Unidos', '1886-05-08'),
+          (12, 'Jonas Gonçalves', 'Lisboa', '1986-05-08'),
           (13, 'Lamborghini', 'Sant Agata Bolognese','1963-10-30'),
           (14, 'Ferrari','	Maranello','1939-09-13'),
           (15, 'Apple', 'California', '1976-04-01');
@@ -21,9 +21,9 @@ INSERT INTO metodospagamento
 	VALUES (0, 'Cartão de Crédito'),
 		   (1, 'PayPal'),
            (2, 'Multibanco'),
-           (3, 'MasterCard'),
-           (4, 'Visa');
-           
+           (3, 'Cartão de Débito'),
+           (4, 'Criptomoedas');
+
 INSERT INTO utilizador_metodosPagamento
 	VALUES (1, 3, 123456789), (1, 4, 132456798),
            (2, 0, 145234789),
@@ -36,7 +36,7 @@ INSERT INTO utilizador_metodosPagamento
            (9, 0, 478937483),
            (10, 4, 743892449),
            (11, 0, 146718467), (11, 1, 743987141), (11, 2, 638742644), (11, 3, 746144664), (11, 4, 461815891),
-		   (12, 0, 111448467), (12, 1, 744242141), (12, 2, 638234244), (12, 3, 746142424), (12, 4, 424225891),
+		   (12, 0, 111448467),
            (13, 0, 146133167), (13, 1, 743913441), (13, 2, 638456166), (13, 3, 746149669), (13, 4, 461815737),
            (14, 0, 146767887), (14, 1, 743988484), (14, 2, 638742277), (14, 3, 746148353), (14, 4, 461826272),
            (15, 0, 146715515), (15, 1, 715115141), (15, 2, 155142644), (15, 3, 745114664), (15, 4, 461819699);
--- Queries.sql
+++ Queries.sql
@@ -10,10 +10,14 @@ SELECT * FROM produto;
 SELECT * FROM produto a
 	WHERE a.quantidade > 0;
     
--- Ver todos os anuncios criados por o utilizador 10
+-- Ver todos os produtos colocados à venda pelo utilizador 10
 SELECT * from produto p, utilizador u 
 	where u.NIF = p.Utilizador_NIF and u.NIF = 10;
     
 -- Ver todas as compras do utilizador 2
 SELECT * FROM compra c, utilizador u 
 	where u.NIF = c.utilizador_NIF and u.NIF = 2;
+
+-- Ver a quantidade que foi vendida de um produto 
+SELECT * from utilizador u, produto p 
+	
