9914d020be9cec73d2b7419cbc091a838f637944
Zaytsev999 <tiago-pinha@hotmail.com>
2018-11-21 18:06:31 +0000

    Arranjar povoamento.

--- Povoamento.sql
+++ Povoamento.sql
@@ -1,21 +1,21 @@
 Use mercado;
 
 INSERT INTO utilizador (NIF, Nome, Morada, DataNascimento, password)
-	VALUES(1 , 'Lucas Silva'      , 'Rua dos Gritos'	   , '2012-01-17', 'LPS2012'),
+	VALUES(1 , 'Lucas Silva'      , 'Rua dos Gritos'	   , '2012-01-17', 'LPS2012' ),
 		  (2 , 'Luís Capa'        , 'Vila Verde'    	   , '1998-12-01', 'LFCC1998'),
-		  (3 , 'Moisés Antunes'   , 'Maria da Fonte'	   , '1999-04-14', 'MAA1999'),
+		  (3 , 'Moisés Antunes'   , 'Maria da Fonte'	   , '1999-04-14', 'MAA1999' ),
 		  (4 , 'Pedro Capa'       , 'Vila Verde'    	   , '1998-12-01', 'PMCC1998'),
-          (5 , 'Tiago Pinheiro'   , 'Vila Verde'     	   , '1998-08-06', 'TMP1998'),
-          (6 , 'Moussa Marega'    , 'Porto'				   , '1991-04-14', 'MM1991'),
-          (7 , 'Rodrigo Rodrigues', 'Braga'				   , '1900-01-01', 'RR1900'),
-          (8 , 'Pedro Proença'    , 'Lisboa'			   , '1970-04-06', 'PP1970'),
-          (9 , 'Gabriel Toledo'   , 'New York'			   , '1991-05-30', 'GT1991'),
-          (10, 'Epitacio de Melo' , 'New York'			   , '1995-07-04', 'EM1995'),
-          (11, 'SAMSUNG'          , 'Seul, Coreia do Sul'  , '1938-01-01', 'S1938'),
-          (12, 'Jonas Gonçalves'  , 'Lisboa'			   , '1986-05-08', 'JG1986'),
-          (13, 'Lamborghini'      , 'Sant, Agata Bolognese', '1963-10-30', 'L1963'),
-          (14, 'Ferrari'          , 'Maranello'			   , '1939-09-13', 'F1939'),
-          (15, 'Apple'            , 'California'		   , '1976-04-01', 'A1976');
+          (5 , 'Tiago Pinheiro'   , 'Vila Verde'     	   , '1998-08-06', 'TMP1998' ),
+          (6 , 'Moussa Marega'    , 'Porto'				   , '1991-04-14', 'MM1991'  ),
+          (7 , 'Rodrigo Rodrigues', 'Braga'				   , '1900-01-01', 'RR1900'  ),
+          (8 , 'Pedro Proença'    , 'Lisboa'			   , '1970-04-06', 'PP1970'  ),
+          (9 , 'Gabriel Toledo'   , 'New York'			   , '1991-05-30', 'GT1991'  ),
+          (10, 'Epitacio de Melo' , 'New York'			   , '1995-07-04', 'EM1995'  ),
+          (11, 'SAMSUNG'          , 'Seul, Coreia do Sul'  , '1938-01-01', 'S1938'   ),
+          (12, 'Jonas Gonçalves'  , 'Lisboa'			   , '1986-05-08', 'JG1986'  ),
+          (13, 'Lamborghini'      , 'Sant, Agata Bolognese', '1963-10-30', 'L1963'   ),
+          (14, 'Ferrari'          , 'Maranello'			   , '1939-09-13', 'F1939'   ),
+          (15, 'Apple'            , 'California'		   , '1976-04-01', 'A1976'   );
           
 INSERT INTO metodospagamento 
 	VALUES (0, 'Cartão de Crédito'),
@@ -67,27 +67,27 @@ INSERT INTO transporte
 
 -- Produtos
 INSERT INTO produto (Designacao, Descricao, Preco, Categoria, NIF, Quantidade)
-	VALUES ('Mochila', 'Mochila cinzenta e nova', '29.99', 'Material escolar', '2', '10'),
-		   ('Relógio', 'Relógio Rolex em ouro', '7000', 'Acessório', '5', '1'),
-		   ('Mesa', 'Mesa Branca', '25', 'Móvel', '2','1'),
-           ('Caderno', 'Caderno de linhas', '0.99', 'Material escolar', '3','50'),
-           ('Quadro Mona Lisa', 'Mona Lisa, de Leonardo da Vinci', '1000', 'Arte', '6','1'),
-           ('Conjunto de canetas', 'Canetas de cor azul', '10.25', 'Material escolar', '8', '50'),
-           ('Casaco', 'Casaco Branco', '99.99', 'Vestuário', '6', '5'),
-           ('Camisola', 'Camisola do Futebol Clube do Porto', '79.99', 'Equipamento desportivo', '8','1'),
-           ('Cachecol', 'Cachecol do Futebol Clube do Porto com assinatura de Marega', '19.89', 'Vestuário',  '6','5'),
-           ('Borracha', 'Borracha branca da marca STAEDTLER', '0.45', 'Material escolar', '2','20'),
-           ('Carteira', 'Carteira Tommy Hilfiger', '150', 'Acessório', '5', '3'),
-           ('Busto Cristiano Ronaldo', 'Busto do melhor jogador do mundo de futebol', '1.99', 'Arte', '5', '1'),
-           ('Camisola', 'Camisola azul e branca', '79.99', 'Vestuário', '5', '3'),
-           ('Caneta de minas', 'Lapiseira Rotring', '0.7', 'Material escolar', '6', '5'),
-           ('Relógio Swatch', 'Relógio de luxo Swatch', '2500', 'Acessório', '5', '1'),
-           ('T-Shirt TH', 'T-Shirt Tommy Hilfiger branca', '60.99', 'Vestuário', '2','3'),
-           ('Samsung Galaxy s8', 'Smartphone em 2ª mão', '150.99', 'Smartphone', '11', '1500'),
-           ('Fones Samsung', 'Fones de ouvido Samsung', '12.29', 'Técnologia', '11', '150'),
-           ('Pintura A Última Ceia', 'Pintura Leonardo da Vinci', '50000', 'Arte', '1', '1'),
-           ('Samsung Galaxy Note9', 'Smartphone preto e novo', '999.99', 'Smartphone', '11', '100');
-
+	VALUES ('Mochila'				 , 'Mochila cinzenta e nova'		   , '29.99' , 'Material escolar'	   , '2' , '10'  ),
+		   ('Relógio'				 , 'Relógio Rolex em ouro'			   , '7000'	 , 'Acessório'		 	   , '5' , '1'   ),
+		   ('Mesa'					 , 'Mesa Branca'					   , '25'    , 'Móvel'			 	   , '2' , '1'   ),
+           ('Caderno'				 , 'Caderno de linhas'				   , '0.99'  , 'Material escolar'	   , '3' , '50'  ),
+           ('Quadro Mona Lisa'		 , 'Mona Lisa, de Leonardo da Vinci'   , '1000'  , 'Arte'			 	   , '6' , '1'   ),
+           ('Conjunto de canetas'	 , 'Canetas de cor azul'			   , '10.25' , 'Material escolar'	   , '8' , '50'  ),
+           ('Casaco'				 , 'Casaco Branco'					   , '99.99' , 'Vestuário'		 	   , '6' , '5'   ),
+           ('Camisola'				 , 'Camisola de Portugal'			   , '79.99' , 'Equipamento desportivo', '8' , '1'   ),
+           ('Cachecol'				 , 'Cachecol de FPF'				   , '19.89' , 'Vestuário'			   , '6' , '5'   ),
+           ('Borracha'				 , 'Borracha branca da marca STAEDTLER', '0.45'  , 'Material escolar'	   , '2' , '20'  ),
+           ('Carteira'				 , 'Carteira Tommy Hilfiger'	       , '150'	 , 'Acessório'		       , '5' , '3'   ),
+           ('Busto Cristiano Ronaldo', 'Miniatura do busto original'       , '1.99'  , 'Arte'				   , '5' , '1'   ),
+           ('Camisola'				 , 'Camisola azul e branca'	           , '79.99' , 'Vestuário'			   , '5' , '3'   ),
+           ('Caneta de minas'		 , 'Lapiseira Rotring'			       , '0.7'   , 'Material escolar'	   , '6' , '5'   ),
+           ('Relógio Swatch'		 , 'Relógio de luxo Swatch'		       , '2500'  , 'Acessório'			   , '5' , '1'   ),
+           ('T-Shirt TH'			 , 'T-Shirt Tommy Hilfiger branca'	   , '60.99' , 'Vestuário'			   , '2' , '3'   ),
+           ('Samsung Galaxy s8'	     , 'Smartphone em 2ª mão'			   , '150.99', 'Smartphone'			   , '11', '1500'),
+           ('Fones Samsung'		   	 , 'Fones de ouvido Samsung'		   , '12.29' , 'Técnologia'		       , '11', '150' ),
+           ('Pintura A Última Ceia'	 , 'Pintura Leonardo da Vinci'		   , '50000' , 'Arte'				   , '1' , '1'   ),
+           ('Samsung Galaxy Note9' 	 , 'Smartphone preto e novo'  		   , '999.99', 'Smartphone'			   , '11', '100' );
+	
 -- Carrinho
 INSERT INTO carrinho (data, trans, NIF)
 	VALUES ('2018-11-15','DHL',5),
