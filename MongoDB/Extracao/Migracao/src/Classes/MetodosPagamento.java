a8bc1205fc3d660876a901d75661c5998aaac301
Moisés Antunes <36673656+MoisesA14@users.noreply.github.com>
2019-01-11 23:00:36 +0000

    Métodos de Pagamento feito

---
+++ MongoDB/Extracao/Migracao/src/Classes/MetodosPagamento.java
@@ -0,0 +1,65 @@
+package Classes;
+
+public class MetodosPagamento {
+
+	private String id;
+	private String designacao;
+
+	public MetodosPagamento(){
+		id = "";
+		designacao = "";
+	}
+
+	public MetodosPagamento(String id, String designacao){
+		this.id = id;
+		this.designacao = designacao;
+	}
+
+	public MetodosPagamento(MetodosPagamento umMetodo){
+        this.id = umMetodo.getId();
+        this.designacao = umMetodo.getDesignacao();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesignacao() {
+    	return designacao;
+    }
+
+    public void setDesignacao(String designacao){
+    	this.designacao = designacao;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) 
+            return false;
+        MetodosPagamento
+         other = (MetodosPagamento) obj;
+        return (this.id.equals(other.getId()) && this.designacao.equals(other.getDesignacao()));
+    }
+    
+    public MetodosPagamento clone(){
+        return new MetodosPagamento (this);
+    }
+
+    @Override
+    public String toString() {
+        return "Método de Pagamento{" + "id=" + id + ", designacao=" + designacao + '}';
+    }
+
+}
---
+++ MongoDB/Extracao/Migracao/src/ClassesDAO/MetodosPagamentoDAO.java
@@ -0,0 +1,67 @@
+package ClassesDAO;
+
+import Classes.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+public Class MetodosPagamentoDAO {
+
+	private Connection conn;
+
+	public MetodosPagamentoDAO(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String connectionUrl = "jdbc:mysql://localhost/configurafacil?" + "user=LFCC&password=55luis14&useSSL=false";
+            this.conn = DriverManager.getConnection(connectionUrl);
+        }
+        catch(ClassNotFoundException | SQLException exc){}
+	}
+
+    public Collection<MetodosPagamento> values(){
+        Collection<MetodosPagamento> metodosPagamento = new HashSet<>();
+        Set<String> keys = keySet();
+        for(String s: keys){
+            MetodosPagamento mp = this.get(s);
+            metodosPagamento.add(mp);
+        }
+        return metodosPagamento;
+    }
+    
+    public Set<String> keySet() {
+        try{
+            Set<String> ids = new HashSet<>();
+            Statement st = conn.createStatement();
+            String str = "Select id From MetodosPagamento";
+            ResultSet res = st.executeQuery(str);
+            while(res.next()){
+                ids.add(res.getString("id"));
+            }
+            return ids;
+        }
+        catch(SQLException exc){throw new NullPointerException(exc.getMessage());}
+    }
+
+	public MetodosPagamento get(Object key) {
+        try {
+            MetodosPagamento metodosPagamento = null;
+            Statement stm = conn.createStatement();
+            String sql = "SELECT * FROM MetodosPagamento WHERE id='"+(String)key+"'";
+            ResultSet rs = stm.executeQuery(sql);
+            if (rs.next()){
+                metodosPagamento = new MetodosPagamento();
+                metodosPagamento.setId(rs.getString("id"));
+                metodosPagamento.setDesignacao(rs.getString("Designacao"));
+            }
+            return metodosPagamento;
+        }
+        catch (NumberFormatException | SQLException e) {throw new NullPointerException(e.getMessage());}
+    }
+
+
+}
