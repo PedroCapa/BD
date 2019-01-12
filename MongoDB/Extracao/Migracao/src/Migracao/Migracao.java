2bdf9691995e3ad23a79c997fec02f0f7a6ba029
Zaytsev999 <tiago-pinha@hotmail.com>
2019-01-12 13:51:44 +0000

    Conecao ao Mongo.

--- MongoDB/Extracao/Migracao/src/Migracao/Migracao.java
+++ MongoDB/Extracao/Migracao/src/Migracao/Migracao.java
@@ -1,6 +1,10 @@
 package Migracao;
 
 import com.mongodb.*;
+import com.mongodb.client.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bson.Document;  
 
 public class Migracao {
 
@@ -9,18 +13,24 @@ public class Migracao {
      */
     public static void main(String[] args) {
         try{
+            Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+            mongoLogger.setLevel(Level.SEVERE); 
+                
             MongoClient mc = new MongoClient("localhost", 27017);
-            DB db = mc.getDB("test");
+            MongoDatabase db = mc.getDatabase("local");
             System.out.println("MongoDb connected");
         
-            DBCollection coll = db.getCollection("test");
-            DBCursor cursor = coll.find();
-        
-            while(cursor.hasNext()){
-                int i = 1;
-                System.out.println(cursor.next());
-                i++;
-            }
+            MongoCollection<Document> collection = db.getCollection("startup_log"); 
+            System.out.println("Collection sampleCollection selected successfully");
+
+            Document document = new Document("title", "MongoDB") 
+                                .append("id", 1)
+                                .append("description", "database") 
+                                .append("likes", 100) 
+                                .append("url", "http://www.tutorialspoint.com/mongodb/") 
+                                .append("by", "tutorials point");  
+                                collection.insertOne(document); 
+                                System.out.println("Document inserted successfully");
         }
         catch(Exception e){
             System.out.println(e);
