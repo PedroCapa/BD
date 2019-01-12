85e33ca9ac0a7545506319813a8b36d4829d199d
LFCC98 <a81960@alunos.uminho.pt>
2019-01-12 13:08:47 +0000

    Fiz os DAO

--- MongoDB/Extracao/Migracao/src/ClassesDAO/MetodosPagamentoDAO.java
+++ MongoDB/Extracao/Migracao/src/ClassesDAO/MetodosPagamentoDAO.java
@@ -14,10 +14,10 @@ public class MetodosPagamentoDAO {
 
 	private Connection conn;
 
-	public MetodosPagamentoDAO(){
+	public MetodosPagamentoDAO(String userName, String pass){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            String connectionUrl = "jdbc:mysql://localhost/configurafacil?" + "user=LFCC&password=55luis14&useSSL=false";
+            String connectionUrl = "jdbc:mysql://localhost/configurafacil?" + "user="+userName+"&password="+pass+"&useSSL=false";
             this.conn = DriverManager.getConnection(connectionUrl);
         }
         catch(ClassNotFoundException | SQLException exc){}
