c69fabaf7573b9eb3bffd159f7f50e8ea12c3ee8
Zaytsev999 <tiago-pinha@hotmail.com>
2019-01-12 14:19:52 +0000

    Criar coleções e DB.

--- MongoDB/Extracao/Migracao/src/Migracao/Migracao.java
+++ MongoDB/Extracao/Migracao/src/Migracao/Migracao.java
@@ -4,24 +4,31 @@ import com.mongodb.*;
 import com.mongodb.client.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.bson.Document;  
+import org.bson.Document;
 
 public class Migracao {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        try{
+        try {
             Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
             mongoLogger.setLevel(Level.SEVERE); 
                 
             MongoClient mc = new MongoClient("localhost", 27017);
-            MongoDatabase db = mc.getDatabase("local");
-            System.out.println("MongoDb connected");
-        
-            MongoCollection<Document> collection = db.getCollection("startup_log"); 
-            System.out.println("Collection sampleCollection selected successfully");
+            MongoDatabase db = mc.getDatabase("Mercado");
+            System.out.println("Conectado ao MongoDB");
+
+            MongoCollection<Document> carrinho = db.getCollection("Carrinho"); 
+            System.out.println("Tenho um carrinho."); 
+            MongoCollection<Document> produto = db.getCollection("Produto"); 
+            System.out.println("Tenho um produto."); 
+            MongoCollection<Document> transporte = db.getCollection("Transporte"); 
+            System.out.println("Tenho um transporte.");
+            MongoCollection<Document> compra = db.getCollection("Compra"); 
+            System.out.println("Tenho uma compra."); 
+            MongoCollection<Document> utilizador = db.getCollection("Utilizador"); 
+            System.out.println("Tenho um utilizador.");
+            MongoCollection<Document> metodos = db.getCollection("Metodos_Pagamento"); 
+            System.out.println("Tenho metodos de pagamento.");
 
             Document document = new Document("title", "MongoDB") 
                                 .append("id", 1)
@@ -29,7 +36,7 @@ public class Migracao {
                                 .append("likes", 100) 
                                 .append("url", "http://www.tutorialspoint.com/mongodb/") 
                                 .append("by", "tutorials point");  
-                                collection.insertOne(document); 
+                                carrinho.insertOne(document); 
                                 System.out.println("Document inserted successfully");
         }
         catch(Exception e){
