49dcfd4b4c70c6f77b5e324f1405ebd993bd7b26
PedroCapa <36596171+PedroCapa@users.noreply.github.com>
2019-01-12 11:39:59 +0000

    Coneção com mongoDb feita

--- MongoDB/Extracao/Migracao/src/Migracao/Migracao.java
+++ MongoDB/Extracao/Migracao/src/Migracao/Migracao.java
@@ -1,12 +1,30 @@
 package Migracao;
 
+import com.mongodb.*;
+
 public class Migracao {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try{
+            MongoClient mc = new MongoClient("localhost", 27017);
+            DB db = mc.getDB("test");
+            System.out.println("MongoDb connected");
         
-    }
-    
+            DBCollection coll = db.getCollection("test");
+            DBCursor cursor = coll.find();
+        
+            while(cursor.hasNext()){
+                int i = 1;
+                System.out.println(cursor.next());
+                i++;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }    
 }
